20821c12a54f6ddf8d9c641a13660c794b34ed48
Christoffer Ljungqvist <christoffer.ljungqvist@hotmail.se>
2018-06-08 16:01:24 +0200

    Fixed so you can create a question

--- Labb4MVC/ClientApp/components/Labb4/CreateQuestion.tsx
+++ Labb4MVC/ClientApp/components/Labb4/CreateQuestion.tsx
@@ -16,23 +16,23 @@ export class CreateQuestion extends React.Component<RouteComponentProps<{}>>{
                 <form onSubmit={this.handleSubmit}>
                     <div id="create-fraga">
                         <label>Fråga:</label>
-                        <input id="QuestionContent" name="QuestionContent" type="text"></input>
+                        <input id="Question" name="Question" type="text"></input>
                     </div>
                     <div id="create-fraga-alternativ-1">
                         <label className="createQuestionLabel">Alternativ 1: </label>
-                        <input id="Alternative1" name="Alternative1" type="text"></input>
+                        <input id="Alternativ1" name="Alternativ1" type="text"></input>
                     </div>
                     <div id="create-fraga-alternativ-2">
                         <label className="createQuestionLabel">Alternativ 2: </label>
-                        <input id="Alternative2" name="Alternative2" type="text"></input>
+                        <input id="Alternativ2" name="Alternativ2" type="text"></input>
                     </div>
                     <div id="create-fraga-alternativ-3">
                         <label className="createQuestionLabel">Alternativ 3: </label>
-                        <input id="Alternative3" name="Alternative3" type="text"></input>
+                        <input id="Alternativ3" name="Alternativ3" type="text"></input>
                     </div>
                     <div id="create-fraga-alternativ-4">
                         <label className="createQuestionLabel">Alternativ 4: </label>
-                        <input id="Alternative4" name="Alternative4" type="text"></input>
+                        <input id="Alternativ4" name="Alternativ4" type="text"></input>
                     </div>
                     <div id="create-fraga-correct-answer">
                         <label className="createQuestionLabel">Rätt svar: </label>
